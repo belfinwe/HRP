129cb1aa2ff7c7db174df527cfc2b7a3a5984ad6
joakimRN <joakimrn02@gmail.com>
2021-06-09 11:32:00 +0200

    Fix and changes in config.py.
    Altered var names to reflect what they are configs of, and fixed an issue with keys from the ini file.

--- app/config.py
+++ app/config.py
@@ -7,14 +7,15 @@ __version__ = "0.1.2"
 # Database connection
 config = configparser.ConfigParser()
 config.read("secret_config.ini")
+db_cfg = config["database"]
 
-user = config["user"]
-password = config["password"]
-postgresserver = f"""{config["password"]}:{config["port"]}"""
-db = config["db_name"]
-driver = config["driver"]
+pg_db_user = db_cfg["user"]
+pg_db_password = db_cfg["password"]
+pg_db_postgresserver = f"""{db_cfg["password"]}:{db_cfg["port"]}"""
+pg_db_name = db_cfg["db_name"]
+pg_db_driver = db_cfg["driver"]
 
-SQLALCHEMY_DATABASE_URL = f"{driver}://{user}:{password}@{postgresserver}/{db}"
+SQLALCHEMY_DATABASE_URL = f"{pg_db_driver}://{pg_db_user}:{pg_db_password}@{pg_db_postgresserver}/{pg_db_name}"
 
 # Log
 __format = "%(asctime)s - %(funcName)s: %(message)s"
